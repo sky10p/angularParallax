7f1b0ab3ae248b6260e79bb32db388c7bb2d189f
Pablo Guijarro Ramírez <pablo@MacBook-Pro-de-Pablo.local>
2016-09-19 00:15:26 +0200

    Implementando la directiva de animaciones

--- public_html/app/angularParallax.js
+++ public_html/app/angularParallax.js
@@ -6,6 +6,46 @@
 
 
 var templateSkParallax='<div><ng-transclude></ng-transclude></div>';
+var templateParallaxAnim='<div class=\'skParallaxAnim\'><ng-transclude></ng-transclude></div>';
+
+var isInsideView=function(elemento){
+    var window_height = $(window).height();
+  var window_top_position = $(window).scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  
+  
+    var $element = $(elemento);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else {
+      $element.removeClass('in-view');
+    }
+  
+};
+
+
+
+var skParallaxAnim=function(){
+    return{
+        restrict:'E',
+        template: templateParallaxAnim,
+        replace:true,
+        transclude:true,
+        link:function(scope, element, attrs){
+            
+        },
+        controller: function($scope){
+            $scope.isInsideView=isInsideView;
+        }
+    };
+}
 
 var skParallax=function(){
     return {
@@ -37,3 +77,5 @@ var skParallax=function(){
 var angularParallax=angular.module('angularParallax',[]);
 
 angularParallax.directive('skParallax',skParallax);
+
+angularParallax.directive('skParallaxAnim',skParallaxAnim);
